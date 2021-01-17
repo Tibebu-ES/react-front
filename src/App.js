2116af711d6b537b5fe1fbf053e83115a98e4383
Tibebu-ES <tibebuenyew@gmail.com>
2021-01-17 18:20:43 +0300

    React routing implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
@@ -8,11 +10,17 @@ import FooterComponent from './components/FooterComponent';
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <div className="container">
-        <ListEmployeeComponent />
-      </div>
-      <FooterComponent />
+      <Router>
+          <HeaderComponent />
+          <div className="container">
+            <Switch>
+              <Route path = "/" component = {ListEmployeeComponent}></Route>
+              <Route path = "/employees" component = {ListEmployeeComponent}></Route>
+               <ListEmployeeComponent />
+            </Switch>
+          </div>
+          <FooterComponent />
+      </Router>
     </div>
     
   );
